2e7c4913884cc3f87adc1510285961f8043bcaa3
Anish Singhania <a.singhania@samsung.com>
2026-05-23 16:06:30 +0530

    Deployment update

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 
 from openai import AzureOpenAI
 from dotenv import load_dotenv
@@ -7,13 +8,37 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+API_KEY = (
+    st.secrets.get(
+        "AZURE_OPENAI_API_KEY",
+        os.getenv("AZURE_OPENAI_API_KEY")
+    )
+)
+
+ENDPOINT = (
+    st.secrets.get(
+        "AZURE_OPENAI_ENDPOINT",
+        os.getenv("AZURE_OPENAI_ENDPOINT")
+    )
+)
+
+API_VERSION = (
+    st.secrets.get(
+        "AZURE_OPENAI_API_VERSION",
+        os.getenv("AZURE_OPENAI_API_VERSION")
+    )
+)
+
+DEPLOYMENT_NAME = (
+    st.secrets.get(
+        "AZURE_GPT_DEPLOYMENT",
+        os.getenv("AZURE_GPT_DEPLOYMENT")
+    )
 )
 
 
-DEPLOYMENT_NAME = os.getenv(
-    "AZURE_GPT_DEPLOYMENT"
+client = AzureOpenAI(
+    api_key=API_KEY,
+    api_version=API_VERSION,
+    azure_endpoint=ENDPOINT
 )
